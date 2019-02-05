55bc578a767f62ba25953aab90b0d27d4a5c33f2
lance <lkditshwane@gmail.com>
2019-02-05 07:24:43 +0200

    right'

---
+++ JavaApplication3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication3;
+
+/**
+ *
+ * @author LDitshwane
+ */
+public class JavaApplication3 {
+
+    
+    public static void main(String[] args) {
+       
+        
+        
+        System.out.println("i win");
+        
+        
+        
+    }
+    
+}
