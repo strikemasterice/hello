9a4564108ee2d153824421f6644336d45959cc0c
lance <lkditshwane@gmail.com>
2019-02-05 07:43:15 +0200

    modif

--- JavaApplication3.java
+++ JavaApplication3.java
@@ -16,9 +16,9 @@ public class JavaApplication3 {
     public static void main(String[] args) {
        
         
-        
+        for(int x = 0;x<5;x++){
         System.out.println("i win");
-        
+        }
         
         
     }
